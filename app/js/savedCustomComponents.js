a95509b7c3df058860d44d8e9d83420144d9c05c
wellhausen <3402403+wellhausen@users.noreply.github.com>
2021-05-04 15:25:20 -0600

    update paths, move folders to app directory

--- app/js/savedCustomComponents.js
+++ app/js/savedCustomComponents.js
@@ -1,27 +1,27 @@
 let savedCustomComponents = [];
 
 function saveCustomComponent(component) {
-    const clone = cloneComponent(component);
-    clone.name = component.name;
-    savedCustomComponents.push(clone);
-    toolbar.message("Saved component " + component.name);
+  const clone = cloneComponent(component);
+  clone.name = component.name;
+  savedCustomComponents.push(clone);
+  toolbar.message("Saved component " + component.name);
 }
 
 function saveCustomComponents() {
-    const stringified = stringify(savedCustomComponents);
-    fs.writeFileSync(
-        __dirname + "/../data/customcomponents.json",
-        stringified,
-        "utf-8"
-    );
+  const stringified = stringify(savedCustomComponents);
+  fs.writeFileSync(
+    //__dirname + "/../data/customcomponents.json",
+    __dirname + "/app/data/customcomponents.json",
+    stringified,
+    "utf-8"
+  );
 }
 
 function getCustomComponents() {
-    const data = fs.readFileSync(
-        __dirname + "/../data/customcomponents.json",
-        "utf-8"
-    );
+  const data = fs.readFileSync(
+    __dirname + "/app/data/customcomponents.json",
+    "utf-8"
+  );
 
-    savedCustomComponents = parse(data).components;
+  savedCustomComponents = parse(data).components;
 }
-
